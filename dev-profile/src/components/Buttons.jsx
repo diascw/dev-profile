e044fb72b1cee6895188e3a412ff5e1be99621cc
Wanessa Dias <130778028+diascw@users.noreply.github.com>
2024-04-28 13:53:38 -0300

    Buttons.jsx
    aprimorando

--- dev-profile/src/components/Buttons.jsx
+++ dev-profile/src/components/Buttons.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import styles from './buttons.module.css';
 
 function Buttons() {
+
+  const links = [
+    { text: 'Quem sou eu?', url: '#' },
+    { text: 'GitHub', url: 'https://github.com/diascw' },
+    { text: 'Linkedin', url: 'https://www.linkedin.com/in/wanessadiascw/' },
+    { text: 'Instagram', url: 'https://www.instagram.com/diascw/' }
+  ];
+
   return (
     <ul className={styles.buttons}>
-      <li>
-        <a href="#">Quem sou eu?</a>
-      </li>
-      <li>
-        <a href="https://github.com/diascw" target="_blank">GitHub</a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/wanessadiascw/" target="_blank">Linkedin</a>
-      </li>
-      <li>
-        <a href="https://www.instagram.com/diascw/" target="_blank">Instagram</a>
-      </li>
+      {links.map((link, index) => (
+        <li key={index}>
+          <a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a>
+        </li>
+      ))}
     </ul>
   );
 }
